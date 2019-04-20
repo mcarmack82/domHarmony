8469156245da1110701b139544ab3c1af693c994
mcarmack82 <michelleco100@gmail.com>
2019-04-20 13:58:56 -1000

    #1 - #9 complete.

--- js/app.js
+++ js/app.js
@@ -2,21 +2,59 @@
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
 
+var divElem = document.getElementById('matCount');
+divElem.innerHTML = 11;
+
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
+
+var divElem = document.getElementById('msgCount');
+divElem.innerHTML = 23;
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
+var divElem = document.getElementById('fullname');
+divElem.innerHTML = 'Ronald McDonald';
+
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
+
+var divElem = document.getElementById('age');
+divElem.innerHTML = '63 Years Old';
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.
 
+
+var newJob = document.createElement('div');
+newJob.id = 'job';
+newJob.innerHTML = 'Clown and Restauranteur';
+data.appendChild(newJob);
+
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
+
+var newHobbies = document.createElement('div');
+newHobbies.id = 'hobbies';
+newHobbies.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.';
+data.appendChild(newHobbies);
 
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
+var newLocation = document.createElement('div');
+newLocation.id = 'location';
+newLocation.innerHTML = 'Honolulu, HI';
+data.appendChild(newLocation);
+
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
 
+var newThurst = document.createElement('div');
+newThurst.id = 'wants';
+newThurst.innerHTML = 'Looking for Mrs. McDonald';
+data.appendChild(newThurst);
+
 //9. In the div id profile section, create a paragraph element with an id of pro2 and create a your own profile for Ronald (at least 2 sentences).
+
+var newPro2 = document.createElement('p');
+newPro2.id = 'pro2';
+newPro2.innerHTML = "Enjoys flipping burgers and eating. It's his passion!";
+data.appendChild(newPro2);
 
 //10. In the div id matches section, target the first div element with the class name of firstName and change Name to Wendy.
 
